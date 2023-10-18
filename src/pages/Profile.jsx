a7e975e1ef40b28cff9648f224e8616b3e00873a
Davincy Project <bdavincy.bd@gmail.com>
2023-10-18 22:41:28 +0700

    changes: restyle profile pages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import axios from "axios";
 
 const Profile = () => {
@@ -29,41 +30,48 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="mt-5 text-white p-3">
-      <div>
-        <ul>
-          <div className="mr-2 flex-1 bg-white bg-opacity-25 mt-5 card justify-content-center align-items-center text-white">
-            <div className="card-body ">My Profile</div>
+    <div
+      className=" d-flex text-white justify-content-center align-items-center"
+      style={{ minHeight: "80vh", marginTop: "5rem" }}
+    >
+      <div className="container-fluid mx-5 rounded p-5 bg-dark">
+        <div className="opacity-75 text-white fs-1 fw-semibold">
+          <div className="text-center text-white">My Profile</div>
+        </div>
+        <center>
+          <div
+            className="mt-4 card center "
+            style={{ width: "7rem", borderRadius: "50%" }}
+          >
+            <img
+              className="poto_kosong"
+              src="/poto_kosong.png"
+              alt="poto_kosong"
+            />
           </div>
-          <center>
-            <div
-              className="mt-4 card center "
-              style={{ width: "7rem", borderRadius: "50%" }}
-            >
-              <img
-                className="poto_kosong"
-                src="/poto_kosong.png"
-                alt="poto_kosong"
-              />
-            </div>
-          </center>
+        </center>
 
-          <div className="d-flex flex-col gap-2">
-            <div>
-              <p className="m-0">Name </p>
-              <div className="mr-2 flex-1 bg-white -25 card justify-content-center align-items-center text-black">
-                <div className="card-body">{profile.name}</div>
-              </div>
-            </div>
-
-            <div>
-              <p className="m-0">Email</p>
-              <div className="mr-2 flex-1 bg-white -25 card justify-content-center align-items-center text-black">
-                <div className="card-body">{profile.email}</div>
+        <div className="d-flex flex-column gap-4 mt-5">
+          <div>
+            <p className="m-0 fs-5">Name </p>
+            <div className="bg-white card text-black text-center">
+              <div className="card-body opacity-75 fw-semibold fs-5">
+                {profile.name}
               </div>
             </div>
           </div>
-        </ul>
+
+          <div>
+            <p className="m-0 fs-5">Email</p>
+            <div className="bg-white card text-black text-center">
+              <div className="card-body opacity-75 fw-semibold fs-5">
+                {profile.email}
+              </div>
+            </div>
+          </div>
+
+          <Button variant="danger">Edit Profile</Button>
+        </div>
       </div>
     </div>
   );
