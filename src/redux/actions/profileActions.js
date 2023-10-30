f389c18cc8fc4c757136c63b74c8f8558923d039
21102035 <21102035@ittelkom-pwt.ac.id>
2023-10-30 21:11:15 +0700

    update

---
+++ src/redux/actions/profileActions.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { setUser } from "../reducers/profileReducers";
+
+export const getUser =
+  (navigate, navigatePathSuccess, navigatePathError) =>
+  async (dispatch, getState) => {
+    try {
+      const { token } = getState().popular;
+
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_VERCEL_AUTH}/me`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      dispatch(setUser(data?.data));
+      if (navigatePathSuccess) navigate(navigatePathSuccess, { replace: true });
+    } catch (err) {
+      if (navigatePathError) navigate(navigatePathError, { replace: true });
+    }
+  };
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import popularMovieReducer from "./popularMovieReducer";
 import searchReducers from "./searchReducers";
+import profileReducers from "./profileReducers";
 
 export default combineReducers({
-    popular: popularMovieReducer,
-    search: searchReducers,
+  popular: popularMovieReducer,
+  search: searchReducers,
+  profil: profileReducers,
 });
